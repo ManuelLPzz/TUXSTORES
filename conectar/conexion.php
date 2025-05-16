56e7f4c422f49a0e50da7add3c6d96ccbd7351d3
ManuelLPzz <manuel.vivo.14@gmail.com>
2025-05-16 00:18:51 -0600

    modificacion 1

--- conectar/conexion.php
+++ conectar/conexion.php
@@ -7,6 +7,6 @@ $database = "apostol";
 $conn = new mysqli($servername, $username, $password, $database);
 
 if ($conn->connect_error) {
-    die("Error de conexión: " . $conn->connect_error);
+    die("Error de conexiónnnn: " . $conn->connect_error);
 }
 ?>
